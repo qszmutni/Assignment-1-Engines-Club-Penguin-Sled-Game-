feca90bc1f06fbd8d2e1ec54d8db6dc81eebc32c
Quinn Szmutni <qszmutni@gmail.com>
2019-10-01 11:43:52 -0400

    Movement
    Transform movement for x like the real game, physics to control the slide speed going down the hill.
    
    Problems: Collision detection might not work due to using transform and physics system at same time, will update if it happens.

---
+++ Assignment 1 Engines/Assets/Movement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody penguin;
+    public float speed = 2.0f;
+    Vector3 vec3;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        penguin = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        vec3 = transform.position;
+
+        float translationX = Input.GetAxis("Vertical");
+        float translationY = Input.GetAxis("Horizontal");
+        
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            vec3 = new Vector3(transform.position.x + 1.0f, vec3.y, vec3.z);
+            transform.position = vec3;
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            vec3 = new Vector3(transform.position.x - 1.0f, vec3.y, vec3.z);
+            transform.position = vec3;
+        }
+
+        penguin.AddForce(0.0f, 0.0f, 1.5f, ForceMode.Force);
+
+
+    }
+}
