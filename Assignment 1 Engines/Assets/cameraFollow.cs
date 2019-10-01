8bebb00cf3114acbd48148e713ec5251a67f880a
Quinn Szmutni <qszmutni@gmail.com>
2019-10-01 12:09:42 -0400

    CameraFollow + Collision

---
+++ Assignment 1 Engines/Assets/cameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class cameraFollow : MonoBehaviour
+{
+
+    public GameObject penguin;
+
+    private Vector3 distance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        distance = transform.position - penguin.transform.position;
+        
+    }
+
+  // Called after each frame.
+    void LateUpdate()
+    {
+        transform.position = penguin.transform.position + distance;
+        
+    }
+}
